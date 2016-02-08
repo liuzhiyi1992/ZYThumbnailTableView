bde40fcb909ab927a5891e38a5682c80ef925dfc
liuzhiyi1992 <55137840@qq.com>
2016-02-08 21:11:44 +0800

    Initial Commit

---
+++ ZYThumbnailTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZYThumbnailTableView
+//
+//  Created by lzy on 16/2/8.
+//  Copyright © 2016年 lzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
