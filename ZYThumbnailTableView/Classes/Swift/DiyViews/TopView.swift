a151bce121cae7d5463db5b9f3804f66c5e4381f
liuzhiyi1992 <55137840@qq.com>
2016-02-28 12:59:33 +0800

    1.增加DiyTopViewDelegate，负责topview中按钮点击后操作

--- ZYThumbnailTableView/Classes/Swift/DiyViews/TopView.swift
+++ ZYThumbnailTableView/Classes/Swift/DiyViews/TopView.swift
@@ -9,23 +9,32 @@
 import UIKit
 
 
+@objc protocol DiyTopViewDelegate {
+    optional func topViewDidClickFavoriteBtn(topView: TopView)
+    optional func topViewDidClickShareBtn(topView: TopView)
+}
+
 //selected A7DA55
 //default  AAAAAA
 class TopView: UIView {
     
     let TAG_BUTTON_GENERAL = 10
     let TAG_BUTTON_SELECTED = 20
+    
+    var indexPath: NSIndexPath!
+    var delegate: DiyTopViewDelegate?
 
-    class func createView() -> TopView? {
+    class func createView(indexPath: NSIndexPath) -> TopView? {
         let view = NSBundle.mainBundle().loadNibNamed("TopView", owner: nil, options: nil).first as? TopView
         guard view != nil else {
             assertionFailure("ERROR: can not load nib \"TopView\"")
             return nil
         }
+        view!.indexPath = indexPath
         return view
     }
     
-    @IBAction func clickDismissButton(sender: UIButton) {
+    @IBAction func clickFavoriteButton(sender: UIButton) {
         
         if sender.tag == TAG_BUTTON_GENERAL {
             sender.setTitleColor(UIColor(red: 167/255.0, green: 218/255.0, blue: 85/255.0, alpha: 1.0), forState: .Normal)
@@ -37,9 +46,15 @@ class TopView: UIView {
             sender.tag = TAG_BUTTON_GENERAL
         }
         
+        //notification
         let notification = NSNotification(name: "NOTIFY_NAME_DISMISS_PREVIEW", object: nil)
         NSNotificationCenter.defaultCenter().performSelector("postNotification:", withObject: notification, afterDelay: 0.25)
-//        NSNotificationCenter.defaultCenter().postNotificationName(NOTIFY_NAME_DISMISS_PREVIEW, object: nil)
+        
+        //delegate
+        if let nonNilDelegate = delegate {
+            nonNilDelegate.topViewDidClickFavoriteBtn?(self)
+        }
+        
     }
     
     
